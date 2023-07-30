a6325fde956313015fec443ac3ddc71a390d4c4e
Miguel Arenas <migue.arenas91@gmail.com>
2023-07-29 20:15:09 -0500

    Layout separation of carts endpoint. missing the bussines logic

--- src/controllers/carts.controller.js
+++ src/controllers/carts.controller.js
@@ -1,39 +1,63 @@
 import cartsService from "../services/carts.services.js";
 
 export async function handleGetAll(req, res, next) {
-  const carts = await cartsService.getAllCarts()
-  res.json(carts)
+  try {
+    const carts = await cartsService.getAllCarts()
+    res.json(carts)
+  } catch (error) {
+    res.json({ errorMsg: error.message });
+  }
 }
 
 export async function handleGetById(req, res, next) {
-  console.log(req.params.cid);
-  const cartById = await cartsService.getCartById(req.params.cid);
-  res.json(cartById);
+  try {
+    console.log(req.params.cid);
+    const cartById = await cartsService.getCartById(req.params.cid);
+    res.json(cartById);
+  } catch (error) {
+    res.json({ errorMsg: error.message });
+  }
 }
 
 export async function handlePost(req, res, next) {
-  const dataNewCart = req.body
-  const userId = req.params.uid //check later
-  const cartCreated = await cartsService.createNewCart(dataNewCart, userId);
-  res.json(cartCreated);
+  try {
+    const dataNewCart = req.body
+    const userId = req.params.uid //check later
+    const cartCreated = await cartsService.createNewCart(dataNewCart, userId);
+    res.json(cartCreated);
+  } catch (error) {
+    res.json({ errorMsg: error.message });
+  }
 }
 
 export async function handlePut(req, res, next) {
-  const cartId = req.params.cid;
-  const newData = req.body;
-  const cartUpdated = await cartsService.updateCart(
-    cartId,
-    newData
-  );
-  res.json(cartUpdated);
+  try {
+    const cartId = req.params.cid;
+    const newData = req.body;
+    const cartUpdated = await cartsService.updateCart(
+      cartId,
+      newData
+    );
+    res.json(cartUpdated);
+  } catch (error) {
+    res.json({ errorMsg: error.message });
+  }
 }
 
 export async function handleDeletebyId(req, res, next) {
-  const cartDeleted = await cartsService.deleteCart(req.params.cid);
-  res.json({ msg: 'The cart Was succesfull deleted'});
+  try {
+    const cartDeleted = await cartsService.deleteCart(req.params.cid);
+    res.json(cartDeleted);
+  } catch (error) {
+    res.json({errorMsg: error.message})
+  }
 }
 
 export async function deleteAllCarts(req, res, next) {
-  await cartsService.deleteAllcarts();
-  res.send(`here we going to delete all existing carts.`);
+  try {
+    const resultOfDelete = await cartsService.deleteAllCarts();
+    res.send(resultOfDelete);
+  } catch (error) {
+    res.json({ errorMsg: error.message });
+  }
 }
--- src/daos/cart/cartsDaoMongoDb.js
+++ src/daos/cart/cartsDaoMongoDb.js
@@ -5,9 +5,27 @@ class CartsDaoMongoDb {
   constructor(daoSelected) {
     this.collection = daoSelected
   }
-
+  async getAllCarts() {
+    return await this.collection.find()
+  }
+  async getCartById(cid) {
+    return await this.collection.findById(cid)
+  }
   async createNewCart(newCartData) {
     return await this.collection.create(newCartData)
+  }
+  async addProductToCart(pid, cid) {
+    //logic 
+  }
+  async updateCart(cid, newData) {
+    return await this.collection.findByIdAndUpdate(cid, { $set: newData }, {new: true})
+  }
+  async deleteCart(cid) {
+    return await this.collection.findByIdAndDelete(cid)
+  }
+
+  async deleteAllCarts() {
+    return await this.collection.deleteMany()
   }
 
 }
